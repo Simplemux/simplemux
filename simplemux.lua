9708ebb7bab412f1e06262a8925438bebc89eca9
Jose Saldana <josemariasaldana@gmail.com>
2021-11-12 18:31:41 +0100

    Distinguish one packet - more than one packet
    I add code in order to count the number of Simplemux separators. If there is a single packet, I can dissect all the subsequent layers. If there is more than one, I will have to show the header and the payload, but not all the details of the payload.

--- simplemux.lua
+++ simplemux.lua
@@ -21,100 +21,181 @@ local protocol_type = ProtoField.uint8("simplemux.Protocol", "Protocol", base.DE
 -- define the field structure of the Simplemux header
 simplemux.fields = { f_SPB, f_LXT, f_LEN, protocol_type }
 
+
+
+
+	
+
+
+
+
+
+
 local data_dis = Dissector.get("data")
 
 -- dissector function
 function simplemux.dissector(buf, pkt, tree)
-	-- put a name in the "protocol" column
-	pkt.cols['protocol'] = "Simplemux"
 
-	-- variable to store the offset: positions I have advanced
-	local offset = 0
 
-	-- first byte
-	-- this is a way to get the most significant bit
-	SPB = ( buf(0,1):uint() - ( buf(0,1):uint() % 128 ) ) / 128
 
-	-- this is a way to get the second bit
-	local value = buf(0,1):uint()
 
-	-- remove the most significant bit
-	if SPB == 1 then
-		value = value - 128
-	end
-	LXT = ( value - (value % 64 ) ) / 64
-
-	-- create the Simplemux subtree
+	-- first, I count the number of packets in the Simplemux bundle
+	local numberOfSimplemuxHeaders = 0
+	local acumLength = 0
+	print()
+	while (acumLength < buf:len()) do
 	
-	-- check if the length has 1 or 2 bytes (depending on LXT, second bit)
-	if LXT == 0 then
-		-- the length field is one byte long
+		-- variable to store the offset: positions I have advanced
+		local prelimOffset = 0
 		
-		-- create the Simplemux protocol tree item
-		-- the second argument of 'buf' means the number of bytes that are considered
-		--a part of simplemux
-		local subtree = tree:add(simplemux, buf(offset,2))
-
-    -- first byte (including SPB, LXT and LEN)
-    subtree:add(f_SPB, SPB)
-		subtree:add(f_LXT, LXT)
-
-		LEN = buf(offset,1):uint() % 64
-		subtree:add(f_LEN, LEN)
-
-		offset = offset + 1
+		-- first byte
+		-- this is a way to get the most significant bit
+		SPB = ( buf(0,1):uint() - ( buf(0,1):uint() % 128 ) ) / 128
 		
-		-- last byte (the second byte): Protocol field
-		Protocol = buf(offset,1):uint()
-	  subtree:add(protocol_type, Protocol)
+		-- this is a way to get the second bit
+		local value = buf(0,1):uint()
+		
+		-- remove the most significant bit
+		if SPB == 1 then
+			value = value - 128
+		end
+		LXT = ( value - (value % 64 ) ) / 64
+		
+		-- check if the length has 1 or 2 bytes (depending on LXT, second bit)
+		if LXT == 0 then
+			-- the length field is one byte long
+			LEN = buf(prelimOffset,1):uint() % 64
+		
+			prelimOffset = prelimOffset + 2
+			acumLength = acumLength + 2 + LEN
+			-- print ("2")
+		else
+			-- the length field is two bytes long
+		
+			-- the length is between the first and the second bytes
+			-- 6 bits come from the first byte (I remove the two most significant ones)
+			-- 7 bits come from the second byte (I remove the most significant one)
+		  LEN = ((buf(prelimOffset,1):uint() % 64 ) * 128 )+ (buf(prelimOffset + 1,1):uint() % 128)
+		
+		  prelimOffset = prelimOffset + 3
+		  acumLength = acumLength + 3 + LEN
+		  -- print ("3")
+
+		end
+		numberOfSimplemuxHeaders = numberOfSimplemuxHeaders + 1
+		print ("total bufLen: " .. buf:len() .. ". LEN: " .. LEN .. ". acumLength: " .. acumLength .. ". packets: " .. numberOfSimplemuxHeaders)
 	
-		offset = offset + 1	
+	end -- end while
+
+	-- use a different approach if there is a Simplemux packet, or
+	-- if there are two or more	
+	if (numberOfSimplemuxHeaders == 1 ) then
+		-- if there is just a Simplemux packet, I can show all the layers using
+		-- the Wireshark dissectors
+
+		-- put a name in the "protocol" column
+		pkt.cols['protocol'] = "Simplemux"
+	
+		-- variable to store the offset: positions I have advanced
+		local offset = 0
+	
+		-- first byte
+		-- this is a way to get the most significant bit
+		SPB = ( buf(0,1):uint() - ( buf(0,1):uint() % 128 ) ) / 128
+	
+		-- this is a way to get the second bit
+		local value = buf(0,1):uint()
+	
+		-- remove the most significant bit
+		if SPB == 1 then
+			value = value - 128
+		end
+		LXT = ( value - (value % 64 ) ) / 64
+	
+		-- create the Simplemux subtree
 		
+		-- check if the length has 1 or 2 bytes (depending on LXT, second bit)
+		if LXT == 0 then
+			-- the length field is one byte long
+			
+			-- create the Simplemux protocol tree item
+			-- the second argument of 'buf' means the number of bytes that are considered
+			--a part of simplemux
+			local subtree = tree:add(simplemux, buf(offset,2))
+	
+	    -- first byte (including SPB, LXT and LEN)
+	    subtree:add(f_SPB, SPB)
+			subtree:add(f_LXT, LXT)
+	
+			LEN = buf(offset,1):uint() % 64
+			subtree:add(f_LEN, LEN)
+	
+			offset = offset + 1
+			
+			-- last byte (the second byte): Protocol field
+			Protocol = buf(offset,1):uint()
+		  subtree:add(protocol_type, Protocol)
+		
+			offset = offset + 1	
+			
+		else
+			-- the length field is two bytes long
+			
+	    -- create the Simplemux protocol tree item
+	    -- the second argument of 'buf' means the number of bytes that are considered
+	    -- a part of simplemux
+	    local subtree = tree:add(simplemux, buf(offset,3))
+	
+			-- first byte (including SPB, LXT and part of LEN)
+	    subtree:add(f_SPB, SPB)
+	    subtree:add(f_LXT, LXT)
+	
+			-- seccond byte (including LXT and the rest of LEN)
+	
+			-- the length is between the first and the second bytes
+			-- 6 bits come from the first byte (I remove the two most significant ones)
+			-- 7 bits come from the second byte (I remove the most significant one)
+	    LEN = ((buf(offset,1):uint() % 64 ) * 128 )+ (buf(offset + 1,1):uint() % 128)
+	    subtree:add(f_LEN, LEN)
+	
+	    offset = offset + 2
+		
+			-- last byte (the third byte): Protocol field
+			Protocol = buf(offset,1):uint()
+		  subtree:add(protocol_type, Protocol)
+		
+			offset = offset + 1	
+		end
+		
+			
+		-- dissect the next content
+	
+		-- if Protocol is 4, there is an IP packet inside
+		if Protocol == 4 then
+			Dissector.get("ip"):call(buf(offset):tvb(), pkt, tree)
+		end
+		-- if Protocol is 143, there is an Eth frame inside
+		if Protocol == 143 then
+	    Dissector.get("eth_withoutfcs"):call(buf(offset):tvb(), pkt, tree)
+		end
+		-- if Protocol is 142, there is a ROHC compressed packet inside
+	  if Protocol == 142 then
+	    Dissector.get("rohc"):call(buf(offset):tvb(), pkt, tree)
+		end
+	
 	else
-		-- the length field is two bytes long
+		-- there is more than a single Simplemux packet
 		
-    -- create the Simplemux protocol tree item
-    -- the second argument of 'buf' means the number of bytes that are considered
-    -- a part of simplemux
-    local subtree = tree:add(simplemux, buf(offset,3))
-
-		-- first byte (including SPB, LXT and part of LEN)
-    subtree:add(f_SPB, SPB)
-    subtree:add(f_LXT, LXT)
-
-		-- seccond byte (including LXT and the rest of LEN)
-
-		-- the length is between the first and the second bytes
-		-- 6 bits come from the first byte (I remove the two most significant ones)
-		-- 7 bits come from the second byte (I remove the most significant one)
-    LEN = ((buf(offset,1):uint() % 64 ) * 128 )+ (buf(offset + 1,1):uint() % 128)
-    subtree:add(f_LEN, LEN)
-
-    offset = offset + 2
-	
-		-- last byte (the third byte): Protocol field
-		Protocol = buf(offset,1):uint()
-	  subtree:add(protocol_type, Protocol)
-	
-		offset = offset + 1	
-	end
-	
+		-- ADD CODE HERE
+		-- SIMPLEMUX
+		--   SEPARATOR
+		--   CONTENT
+		--   SEPTARATOR
+		--   CONTENT		
 		
-	-- dissect the next content
-
-	-- if Protocol is 4, there is an IP packet inside
-	if Protocol == 4 then
-		Dissector.get("ip"):call(buf(offset):tvb(), pkt, tree)
-	end
-	-- if Protocol is 143, there is an Eth frame inside
-	if Protocol == 143 then
-    Dissector.get("eth_withoutfcs"):call(buf(offset):tvb(), pkt, tree)
-	end
-	-- if Protocol is 142, there is a ROHC compressed packet inside
-  if Protocol == 142 then
-    Dissector.get("rohc"):call(buf(offset):tvb(), pkt, tree)
-	end
-end
+	end -- end of if (numberOfSimplemuxHeaders == 1) / else
+	
+end -- end of function simplemux.dissector()
 
 -- load the UDP port table
 local udp_encap_table = DissectorTable.get("udp.port")
